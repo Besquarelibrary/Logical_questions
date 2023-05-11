fed4b5d99174aca4329c2cda062cfa4a7274222f
UDAYKUMAR <uday402403@gmail.com>
2023-05-11 14:53:21 +0530

    To find Perfect number

---
+++ Udaykumar/Perfect_number.py
@@ -0,0 +1,25 @@
+num = int(input("enter a number:"))  # Enter a number to find whether it is perfect number or not
+My_List = []  # Empty list to store the div numbers
+
+# Method -- 1
+for i in range(1, num):  # loop to find div numbers
+    if num % i == 0:  # dividing num with lower range  numbers
+        My_List.append(i)  # adding using append method div numbers into list
+
+if sum(My_List) == num:  # checking num whether it is perfect number or not
+    print(num, "Is a perfect number")
+else:
+    print(num, "Not a perfect number")
+
+
+# Method -- 2
+for i in range(1, num):  # loop to find div numbers
+    if num % i == 0:  # dividing num with lower range  numbers
+        My_List += [i]  # adding div numbers into list by list addition
+
+if sum(My_List) == num:  # checking num whether it is perfect number or not
+    print(num, "Is a perfect number")
+else:
+    print(num, "Not a perfect number")
+
+
