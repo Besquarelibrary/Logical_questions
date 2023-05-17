bbfe2fbdbe46026263f39724e4c5167b613cc552
AnushaChettukindi <116426013+AnushaChettukindi@users.noreply.github.com>
2023-05-18 01:03:06 +0530

    Update keypad_program.py
    Added code in c as well

--- anusha_chettukindi/keypad_program.py
+++ anusha_chettukindi/keypad_program.py
@@ -34,3 +34,65 @@ OUTPUT:
 enter string anusha
 anusha
 266887777442
+-------------------------------------------------------------------------------------------------------------
+LOGIC-2 IN PYTHON
+#input string read from user
+string=input("enter string")
+#list of digits on keypad
+keypad_digits=['2','22','222','3','33','333','4','44','444','5','55','555','6','66','666','7','77','777','7777','8','88','888','9','99','999','9999']
+alp=[]
+#creation of alphabates list
+for i in range(97,123):
+    alp=alp+[chr(i)]
+#to store string of digits
+resultant_digits=""
+#iterate the string
+for i in range(0,len(string)):
+    #iterate the alphabates
+    for j in range(0,len(alp)):
+        #condition to check the presence of char in alphabates
+        if(string[i]==alp[j]):
+           resultant_digits=resultant_digits+keypad_digits[j]
+print(resultant_digits)
+OUTPUT:
+enter string anusha
+anusha
+266887777442
+
+--------------------------------------------------------------------------------------------------------------------------------------
+LOGIC IN C:
+#include <stdio.h>
+
+int main() {
+    char char_array[26];
+    int res_digit[10];
+    //store input string 
+    char input[]="anusha";
+    //for creating alphabates array
+    for(int i=0; i<26; i++){
+        char_array[i] = 'a' + i;
+    }
+    //initialize array with keypad digits
+    int digit_array[26]={2,22,222,3,33,333,4,44,444,5,55,555,6,66,666,7,77,777,7777,8,88,888,9,99,999,9999};
+    //to create the new digit array
+    int g=0;
+    //iterate characters in an input string
+    for(int i=0;input[i]!='\0';i++){
+        //iterate digits array
+        for(int j=0;j<26;j++){
+            //comparing character in the list of alphabates array
+            if(input[i]==char_array[j]){
+                //store that character digit value in the new array
+                res_digit[g]=digit_array[j];
+                g+=1;
+            }
+        }
+    
+    }
+    for(int i=0;i<g;i++){
+        printf("%d",res_digit[i]);
+    }
+    
+}
+OUTPUT:
+266887777442
