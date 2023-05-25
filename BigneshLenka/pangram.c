18c9168167f380677f1b5f0a45217b9e77d83dd8
bignesh <bigneshlenka20@gmail.com>
2023-05-25 12:59:58 +0530

    Added comments in the program

--- BigneshLenka/pangram.c
+++ BigneshLenka/pangram.c
@@ -3,21 +3,21 @@ int main()
 {
 	int i,count[26],found;
 	char str[]="this is Bignesh lenka";
-	for(i=0;str[i]!='\0';i++)
+	for(i=0;str[i]!='\0';i++) //looping for accessing the characters in the string
 	{
 		
-		count[str[i]-'a']++;
+		count[str[i]-'a']++; //logic for counting each character count in the string
 	}
-	found=1;
-	for(i=0;i<26;i++)
+	found=1; //using for condition
+	for(i=0;i<26;i++) // looping to check the count arrray
 	{
-		if(count[i]==0)
+		if(count[i]==0) //condition if the alphabet not being in the string
 		{
-			found=0;
+			found=0; // Using for condition
 			printf("%c ",i+'a');
 		}
 	}
-	if(found)
+	if(found) //condition for pangram
 	{
 		printf("pangram");
 	}
